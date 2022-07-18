117dc3c289b74b63e0de9b3f84659d16b7774e4f
Adrian Valladares <adri@DESKTOP-03C1CDJ.localdomain>
2022-07-18 16:58:38 +0200

    created Layout component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from "react";
+
+const Layout = ({ walletAddress }: any): ReactElement => (
+  <div className="header-container">
+    <p className="header">🏖 Summer Time Animations 🏖</p>
+    <p className="sub-text">View your GIF collection in the metaverse ✨</p>
+  </div>
+);
+
+export default Layout;
